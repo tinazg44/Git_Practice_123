cccedba915a68549973523eed050154aaa2be11d
tinazg44 <106931235+tinazg44@users.noreply.github.com>
2022-08-31 09:23:41 -0500

    Update page1.java

--- src/chapter_2/page1.java
+++ src/chapter_2/page1.java
@@ -4,4 +4,5 @@ public class Page1 {
     // Every time after we create a file we need to commit
     // Commit, commit, commit
     // I edit Page1( it was with lowercase)
+    I want to keep this page
 }
